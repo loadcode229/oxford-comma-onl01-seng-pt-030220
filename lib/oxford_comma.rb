984791c6deee4eaaf5d007c5bf82cf3fee0c9303
Adam Priest <sepone229@gmail.com>
2020-03-10 01:20:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,9 @@ def oxford_comma(array)
   if array.size == 1
     return array.join
   end
-  if array.size == 2
-    array(1, " and ").join(" ")
+  case array
+    when array == 2
+      puts array.push(1, " and ").join(" ")
  
   end
 end
