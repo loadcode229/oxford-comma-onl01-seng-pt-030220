9faea0e0b3573519398dd55f2eedb2b29871ea81
Adam Priest <sepone229@gmail.com>
2020-03-10 01:38:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     return array.join(" and ")
   elsif array.size > 2
-    new_last_array_item  = " and #{array[-3]}"
+    new_last_array_item  = " and #{array[-1]}"
     array.pop
     array.push(new_last_array_item)
     return array.join(", ")
